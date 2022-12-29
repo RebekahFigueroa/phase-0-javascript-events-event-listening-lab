cd6629e5cfd8ba02b495ab989dd407a4dd2ed046
Rebekah <figueroarebekah@gmail.com>
2022-12-28 18:21:05 -0600

    commit tree 0d7828c1b1bf3c85cf9393e39ecc304243bb613e

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// const input = document.getElementById("button");
+
+// function addingEventListener() {
+//   alert("I was clicked!");
+// }
+
+// input.addEventListener("click", addingEventListener);
+
 function addingEventListener() {
+  const input = document.getElementById("button");
+
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+
+  input.addEventListener("click", clickAlert);
 }
